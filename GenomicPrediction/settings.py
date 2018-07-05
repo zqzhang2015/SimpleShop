80e27d274633d52005045b93970d50d939fd3a04
dangjoeltang <31787718+dangjoeltang@users.noreply.github.com>
2018-07-05 07:29:14 -0500

    changed STATIC again

--- GenomicPrediction/settings.py
+++ GenomicPrediction/settings.py
@@ -146,7 +146,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    'www/static/',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 
