08ae03fe49e0b79ce6291b8e6ed53c4eb5c2e5cd
dangjoeltang <31787718+dangjoeltang@users.noreply.github.com>
2018-07-05 03:01:31 -0500

    changed STATIC_ROOT again

--- GenomicPrediction/settings.py
+++ GenomicPrediction/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
