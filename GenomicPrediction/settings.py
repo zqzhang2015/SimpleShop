f1b1a86ca3f896a22c5755049ecfdcb556458264
dangjoeltang <31787718+dangjoeltang@users.noreply.github.com>
2018-07-06 20:16:16 -0500

    set debug true

--- GenomicPrediction/settings.py
+++ GenomicPrediction/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 # TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = ['*']
